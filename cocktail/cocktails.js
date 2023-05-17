42fd77495d3ad61dccbf7d5608a844e553029b9f
Correa7 <correadamian2019@gmail.com>
2023-05-17 15:01:25 -0300

    format prittier

--- cocktail/cocktails.js
+++ cocktail/cocktails.js
@@ -1,69 +1,96 @@
-const alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-let str = document.getElementById('str')
-function alpha (arr){
-    str.innerHTML=''
-    for (const e of arr){
-        let alphabetical = document.createElement("div");
-        alphabetical.className = "alphabetical";
-        alphabetical.innerHTML = `
+const alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let str = document.getElementById("str");
+function alpha(arr) {
+  str.innerHTML = "";
+  for (const e of arr) {
+    let alphabetical = document.createElement("div");
+    alphabetical.className = "alphabetical";
+    alphabetical.innerHTML = `
         <button id="${e}" class="letras" >${e.toUpperCase()}</button>
-          `
-        str.append(alphabetical)
-        let button = document.getElementsByClassName('letras')
-        for (btn of button) {
-            btn.addEventListener("click", concat);
-          }
+          `;
+    str.append(alphabetical);
+    let button = document.getElementsByClassName("letras");
+    for (btn of button) {
+      btn.addEventListener("click", concat);
     }
+  }
 }
-alpha(alphabet)
+alpha(alphabet);
 
-function concat (e){
-  e.preventDefault()
-  let url ='https://www.thecocktaildb.com/api/json/v1/1/search.php?f='+ e.target.id
-fetch(url)
+function concat(e) {
+  e.preventDefault();
+  let url =
+    "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=" + e.target.id;
+  fetch(url)
     .then((res) => res.json())
-    .then((data) =>{
-       let {drinks} = data
-       appWeb (drinks)
+    .then((data) => {
+      let { drinks } = data;
+      appWeb(drinks);
     })
-    .catch((error) => console.log(error))
-
+    .catch((error) => console.log(error));
 }
 
-function appWeb (array) {
-    let contenedor= document.getElementById('contenedor')
-    let modal = document.getElementById("myModal");
-    let receta = document.getElementById("receta");
-    const drinks= array
-    renderizar(array)
-    
-    function renderizar(array) {
-        contenedor.innerHTML = "";
-        for (const product of array) {
-          let tarjetabody = document.createElement("div");
-          tarjetabody.className = "tarjeta";
-          tarjetabody.innerHTML = `
+function appWeb(array) {
+  let contenedor = document.getElementById("contenedor");
+  let modal = document.getElementById("myModal");
+  let receta = document.getElementById("receta");
+  const drinks = array;
+  renderizar(array);
+
+  function renderizar(array) {
+    contenedor.innerHTML = "";
+    for (const product of array) {
+      let tarjetabody = document.createElement("div");
+      tarjetabody.className = "tarjeta";
+      tarjetabody.innerHTML = `
             <div class="card-1">
                 <button id='${product.idDrink}' class='btn-img' >
                     <img id='${product.idDrink}' class="card-img-top" src=${product.strDrinkThumb} alt="Card image cap">
                 </button>
                 <div class="">
-                    <h5 class="card-title">${ product.strDrink}</h5>
+                    <h5 class="card-title">${product.strDrink}</h5>
                 </div>
             </div>
-              `
-          contenedor.append(tarjetabody);
-        }
-        let imgModal = document.getElementsByClassName("card-img-top")
-        for (let a of imgModal) {
-            a.addEventListener("click", esconder)
-            a.addEventListener("click", renderizarModal)
-        }
-      }
-    function renderizarModal(e){
-        receta.innerHTML = ""
-        let drink = drinks.find((p)=>p.idDrink == e.target.id)
-        receta.innerHTML = `
+              `;
+      contenedor.append(tarjetabody);
+    }
+    let imgModal = document.getElementsByClassName("card-img-top");
+    for (let a of imgModal) {
+      a.addEventListener("click", esconder);
+      a.addEventListener("click", renderizarModal);
+    }
+  }
+  function renderizarModal(e) {
+    receta.innerHTML = "";
+    let drink = drinks.find((p) => p.idDrink == e.target.id);
+    receta.innerHTML = `
         <span id='close' class="close">&times;</span>
             <div class="card mb-3">
                 <div class="card-body">
@@ -130,20 +157,20 @@ function appWeb (array) {
                     </table>
                 </div>
             </div>
-                ` 
-                let span = document.getElementById("close")
-                span.addEventListener('click', mostrar)        
-    }
+                `;
+    let span = document.getElementById("close");
+    span.addEventListener("click", mostrar);
+  }
 
-    function esconder(e) {
-        modal.style.display = "block";
+  function esconder(e) {
+    modal.style.display = "block";
+  }
+  function mostrar(e) {
+    modal.style.display = "none";
+  }
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
     }
-    function mostrar(e){
-        modal.style.display = "none";
-    }
-    window.onclick = function (event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+  };
 }
